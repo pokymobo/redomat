3789cafacc6a3918d929b22b4b1d092369d5e566
Daniel Golle <daniel@makrotopia.org>
2015-10-20 12:52:44 +0200

    also serve sdimg files

--- libredo/data/serve.sh
+++ libredo/data/serve.sh
@@ -49,6 +49,7 @@ cd /REDO/build/tmp/deploy/images
 
 find /REDO/build/tmp/deploy/images -type l -name "core-image*.iso" -exec ln -s {} "$RESULTDIR/" ';'
 find /REDO/build/tmp/deploy/images -type l -name "core-image*.ova" -exec ln -s {} "$RESULTDIR/" ';'
+find /REDO/build/tmp/deploy/images -type l -name "core-image*.*sdimg" -exec ln -s {} "$RESULTDIR/" ';'
 )
 
 (
